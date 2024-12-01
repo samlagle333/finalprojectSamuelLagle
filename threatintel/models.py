483a5e40c4361e71d12e443c0ba745d73701d33e
samlagle333 <splagle@mail.fhsu.edu>
2024-12-01 14:18:14 -0600

    Further developed the bookmarking functionality

--- threatintel/models.py
+++ threatintel/models.py
@@ -10,4 +10,4 @@ class Threat(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.name
+        return f"{self.name} ({self.severity})"
