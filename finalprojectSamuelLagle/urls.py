918f7e9508fd163f89ea28c9b5e5a3be2f578c00
samlagle333 <splagle@mail.fhsu.edu>
2024-12-01 13:49:13 -0600

    Created logout functionality

--- finalprojectSamuelLagle/urls.py
+++ finalprojectSamuelLagle/urls.py
@@ -1,8 +1,10 @@
-from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
+from django.contrib import admin
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('threatintel.urls')),
-    path('accounts/', include('allauth.urls')),
+    path('', include('threatintel.urls')),  # Include your app URLs
+    path('accounts/', include('allauth.urls')),  # Allauth URLs
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),  # Logout URL
 ]
